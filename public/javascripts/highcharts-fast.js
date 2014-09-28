9b39a3d5f179a3f7bddf9fb81a043f281d6edf32
macadev <danielmacario5@gmail.com>
2014-09-27 20:28:09 -0400

    working temperature plot

--- public/javascripts/highcharts-fast.js
+++ public/javascripts/highcharts-fast.js
@@ -3,31 +3,25 @@ $(function () {
   var new_temperature;
   var init_dates;
   var init_temperatures;
+  var call_id;
 
-  function getInitialTemperatures() {
-    $.ajax({url: 'http://198.100.145.140/wh/get-data.php?a=all_temp', success: function(result){
-      init_temperatures_and_dates = result;
-      console.log(result);
+  function getNewTemperature(series) {
+    console.log('ICI!!!!!!!');
+    $.ajax({url: 'http://198.100.145.140/wh/get-data.php?a=all_temp&last_id=' + call_id, success: function(new_temperatures) {
+      call_id = new_temperatures[new_temperatures.length - 1].id;
+
+      data_set = _.map(new_temperatures, function(new_data) {
+        return { y : parseFloat(new_data.temp), x : new Date(new_data.date_sent) };
+      });
+
+      _.each(data_set, function(data_point) {
+        series.addPoint([data_point.x, data_point.y], true, true);
+      }); 
     }});
-
-    //NOTE: temp might be a string in DB, possible bug here.
-    data = _.map(init_temperatures_and_dates, function(temp_date) {
-      return { x : temp_date.temp, y : temp_date.date_sent };
-    });
-    
-    if (result.length > 20) {
-      return data.splice(data.length - 20, data.length);
-    }
-    return data;  
   }
 
-  function getNewTemperature() {
-    $.ajax({url: ' ', success: function(new_emperature) {
-      new_temperature = new_temperature;
-    }})
-  }
+  function generateTable(data) {
 
-  $(document).ready(function () {
     Highcharts.setOptions({
       global: {
           useUTC: false
@@ -40,16 +34,21 @@ $(function () {
         animation: Highcharts.svg, // don't animate in old IE
         marginRight: 10,
         events: {
+          
           load: function () {
               //select the only series we have
               var series = this.series[0];
               
               //poll every 5 seconds for new temperature to be plotted
-              setInterval(function () {
-                  var x = (new Date()).getTime(), // current time
-                      y = Math.random();
-                  series.addPoint([x, y], true, true);
-              }, 1000);
+              setInterval(getNewTemperature(series), 5000);
+              
+              
+              // setInterval(function () {
+              //     var x = (new Date()).getTime(), // current time
+              //         y = Math.random();
+              //     series.addPoint([x, y], true, true);
+              // }, 5000);
+              
           }
         }
       },
@@ -85,24 +84,23 @@ $(function () {
       },
       series: [{
         name: 'Random data',
-        data: getInitialTemperatures();
-          /*
-          function () {
-              // generate an array of random data
-            var data = [],
-                time = (new Date()).getTime(),
-                i;
-
-            for (i = -19; i <= 0; i += 1) {
-              data.push({
-                x: time + i * 1000,
-                y: Math.random()
-              });
-            }
-            return data;
-          }())
-          */
+        data: data
       }]
     });
+  }
+
+  $(document).ready(function () {
+
+    $.ajax({url: 'http://198.100.145.140/wh/get-data.php?a=all_temp', success: function(result){
+      var result = JSON.parse(result);
+      call_id = result[result.length - 1].id;
+      console.log(result[result.length - 1]);
+      data = _.map(result, function(temp_date) {
+        return { y : parseFloat(temp_date.temp), x : new Date(temp_date.date_sent) };
+      });
+      console.log(data[0]);
+      
+      generateTable(data);
+    }});
   });
 });
